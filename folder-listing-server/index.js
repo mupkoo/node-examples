a815a0572d11af2d561578b52b30dfc9279e7f24
Mirko Akov <mupkoo@gmail.com>
2014-08-02 00:29:49 +0300

    Create the initial folder listing functionality

--- folder-listing-server/index.js
+++ folder-listing-server/index.js
@@ -26,13 +26,53 @@ var server = http.createServer(function (req, res) {
 });
 
 function renderDirectory(res, dir) {
-    var html = templateCache
-        .replace('{{pageTitle}}', dir)
-        .replace('{{folderName}}', dir)
-        .replace('{{files}}', row.replace('{{name}}', 'Yesss'));
+    var dirToRead = path.join(staticRoot, dir);
+    var html = templateCache.replace('{{pageTitle}}', dir).replace('{{folderName}}', dir);
+    var rowsHtml = '';
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(html);
+    fs.readdir(dirToRead, function (err, files) {
+        if (err) return send500();
+
+        var filesLength = files.length;
+        var totalIterations = 0;
+
+        files.forEach(function (file) {
+            var filePath = path.join(dirToRead, file);
+            fs.stat(filePath, function (err, stats) {
+                totalIterations++;
+
+                if (stats.isDirectory()) {
+                    rowsHtml += renderRow({
+                        link: file,
+                        name: file,
+                        type: 'directory',
+                        modifiedAt: '--'
+                    });
+                } else {
+                    rowsHtml += renderRow({
+                        link: file,
+                        name: file,
+                        type: 'document',
+                        modifiedAt: stats.mtime
+                    });
+                }
+
+                if (filesLength <= totalIterations) {
+                    html = html.replace('{{files}}', rowsHtml);
+
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(html);
+                }
+            });
+        });
+    });
+}
+
+function renderRow(data) {
+    return row.replace('{{link}}', data.link)
+        .replace('{{name}}', data.name)
+        .replace('{{type}}', data.type)
+        .replace('{{modifiedAt}}', data.modifiedAt);
 }
 
 server.listen(8001, function () {
