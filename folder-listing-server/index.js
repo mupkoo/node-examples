55e198a68b4b0397b6083447264000a411b495b4
Mirko Akov <mupkoo@gmail.com>
2014-08-02 17:22:17 +0300

    Refactor the logic of the folder server

--- folder-listing-server/index.js
+++ folder-listing-server/index.js
@@ -1,3 +1,4 @@
+// Modules
 var http = require('http');
 var path = require('path');
 var url = require('url');
@@ -6,16 +7,18 @@ var mime = require('mime');
 var moment = require('moment');
 var errors = require('./libs/errors.js');
 
-var templateCache = fs.readFileSync(path.join(__dirname, 'index.html'), { encoding: 'utf8' });
-var row = fs.readFileSync(path.join(__dirname, '_line.html'), { encoding: 'utf8' });
 var staticRoot = path.join(__dirname, 'static');
+
+// Preload the tempaltes
+var templateCache = fs.readFileSync(path.join(__dirname, 'index.html'), { encoding: 'utf8' });
+var rowTemplate = fs.readFileSync(path.join(__dirname, '_row.html'), { encoding: 'utf8' });
 
 var server = http.createServer(function (req, res) {
     var absPath;
     var pathname = url.parse(req.url).pathname;
 
     if (pathname == '/') {
-        renderDirectory(res, '/');
+        parseFolder(res, '');
     } else {
         absPath = path.join(staticRoot, pathname);
 
@@ -25,7 +28,7 @@ var server = http.createServer(function (req, res) {
                     if (err) return errors.send500(res);
 
                     if (stats.isDirectory()) {
-                        renderDirectory(res, pathname);
+                        parseFolder(res, pathname);
                     } else {
                         sendFile(res, pathname);
                     }
@@ -37,57 +40,80 @@ var server = http.createServer(function (req, res) {
     }
 });
 
-function renderDirectory(res, dir) {
-    var absPath = path.join(staticRoot, dir);
-    var html = templateCache.replace('{{pageTitle}}', dir).replace('{{folderName}}', dir);
-    var rowsHtml = '';
+function parseFolder(res, folder) {
+    var absPath = path.join(staticRoot, folder);
 
     fs.readdir(absPath, function (err, files) {
-        if (err) return errors.send500(res);
+        if (err) {
+            errors.send404(res);
+        } else {
+            parseFiles(res, folder, files);
+        }
+    });
+}
 
-        var filesLength = files.length;
-        var totalIterations = 0;
+function parseFiles(res, folder, files) {
+    var absPath = path.join(staticRoot, folder);
+    var data = { files: [], folders: [] };
+    var counter = 0;
+    var filesLength = files.length;
+    var temp, filePath;
 
-        files.forEach(function (file) {
-            var filePath = path.join(absPath, file);
+    files.forEach(function (file) {
+        filePath = path.join(absPath, file);
 
-            fs.stat(filePath, function (err, stats) {
-                totalIterations++;
+        fs.stat(filePath, function (err, stats) {
+            temp = {
+                link: folder + '/' + file,
+                name: file
+            };
 
-                if (stats.isDirectory()) {
-                    rowsHtml += renderRow({
-                        link: file,
-                        name: file,
-                        type: 'directory',
-                        modifiedAt: '--'
-                    });
-                } else {
-                    rowsHtml += renderRow({
-                        link: file,
-                        name: file,
-                        type: 'document',
-                        modifiedAt: stats.mtime
-                    });
-                }
+            if (stats.isDirectory()) {
+                temp.type = 'directory';
+                temp.modifiedAt = '';
+                temp.modifiedAgo = '--';
 
-                if (filesLength <= totalIterations) {
-                    html = html.replace('{{files}}', rowsHtml);
+                data.folders.push(temp);
+            } else {
+                temp.type = 'document';
+                temp.modifiedAt = stats.mtime;
+                temp.modifiedAgo = moment(stats.mtime).fromNow();
 
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.end(html);
-                }
-            });
+                data.files.push(temp);
+            }
+
+            // Render the folder if this is the last file
+            counter++;
+            if (filesLength <= counter) {
+                renderFolder(res, folder, data);
+            }
         });
     });
 }
 
+function renderFolder(res, folder, data) {
+    var rows = '';
+
+    data.folders.concat(data.files).forEach(function (record) {
+        rows += renderRow(record);
+    });
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(
+        templateCache
+            .replace('{{pageTitle}}', folder)
+            .replace('{{folderName}}', folder)
+            .replace('{{rows}}', rows)
+    );
+}
+
 function renderRow(data) {
-    return row
+    return rowTemplate
         .replace('{{link}}', data.link)
         .replace('{{name}}', data.name)
         .replace('{{type}}', data.type)
-        .replace('{{fullDate}}', data.modifiedAt)
-        .replace('{{modifiedAt}}', moment(data.modifiedAt).fromNow());
+        .replace('{{modifiedAt}}', data.modifiedAt)
+        .replace('{{modifiedAgo}}', data.modifiedAgo);
 }
 
 function sendFile(res, file) {
